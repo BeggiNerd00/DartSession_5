2bbb07bc49e6ab63e21062a87f7e83a5fa56da75
BeggiNerd00 <bjartmar.hallgrimsson@gmail.com>
2021-10-13 17:36:00 +0000

    upgradefrom_yt1

--- bin/bossChallenge1/yt2.dart
+++ bin/bossChallenge1/yt2.dart
@@ -9,4 +9,19 @@ import 'dart:io';
 
 void main() {
 
+  print('Please input a number between 2 and 10: ');
+  int step0 = int.parse(stdin.readLineSync());
+
+  int step1 = step0 * 2;
+
+  if (step0 > 2 && step0 < 10) {
+    print('$step0 is a valid number, thank you for playing');
+
+    print('\nHere is your number multiplied by 2...');
+    print('$step0 * 2 = $step1');
+
+  } else {
+    print('Not a valid number for this programs request.');
+
+  }
 }
